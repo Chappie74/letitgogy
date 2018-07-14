59be14c65991ab99684a20ee00b2040657d32629
Chappie74 <seanmsingh7@gmail.com>
2018-07-14 14:34:22 -0400

    changed the database connection string to that of the database that is going to be  used for this project.

--- letitgogy/Models/IdentityModels.cs
+++ letitgogy/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace letitgogy.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("letitgogydbconn", throwIfV1Schema: false)
         {
         }
 
